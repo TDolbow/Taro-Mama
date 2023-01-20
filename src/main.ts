abc30133f204d8de74437fdf76da462ab15322de
WillHart2002 <wilhart@udel.edu>
2023-01-20 09:32:40 -1000

    added default phaser game vite template

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import Phaser from 'phaser'
+
+import FirstScene from './FirstScene'
+
+const config: Phaser.Types.Core.GameConfig = {
+	type: Phaser.AUTO,
+	parent: 'app',
+	width: 800,
+	height: 600,
+	physics: {
+		default: 'arcade',
+		arcade: {
+			gravity: { y: 200 },
+		},
+	},
+	scene: [FirstScene],
+}
+
+export default new Phaser.Game(config)
