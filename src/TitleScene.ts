c36ee918c3c1c6bccb7be7debb6e09cc9a1eb67e
RiceViz <tyranricejr@gmail.com>
2023-01-26 11:23:16 -1000

    Added Game Winning Sound

--- src/TitleScene.ts
+++ src/TitleScene.ts
@@ -15,6 +15,8 @@ export default class TitleScene extends Phaser.Scene {
 		this.load.audio("clicksound", ["assets/sounds/mixkit-select-click-1109.wav"]);
 		this.load.audio("jazz1", ["assets/sounds/mixkit-chill-bro-494.mp3"]);
 		this.load.audio("completedRecipe", ["assets/sounds/mixkit-instant-win-2021.wav"]);
+		this.load.audio("sceneCompletion", ["assets/sounds/mixkit-video-game-treasure-2066.wav"]);
+		this.load.audio("levelCompletion", ["assets/sounds/mixkit-game-level-completed-2059.wav"]);
 
 		//for mute button
 		this.load.image('mute', 'assets/buttons/mute.png')
