eb5fd7f14e5e0dc2120db7bec989cb72cb19f6a2
Trevor Dolbow <tdolbow@udel.edu>
2023-01-26 22:11:08 -1000

    Removed console.logs

--- src/CheckPositions.ts
+++ src/CheckPositions.ts
@@ -2,8 +2,6 @@ import Phaser from 'phaser';
 
 export function CheckPositions(name_order: Phaser.GameObjects.GameObject[]){
     const name_order_copy = name_order;
-    console.log("Before sort - name_order");
-    console.log(name_order);
 
     //Sort elements by their y value.
     const sortedByHeight:Phaser.GameObjects.GameObject[] = name_order_copy.slice(0).sort((n1,n2) =>{
@@ -18,18 +16,11 @@ export function CheckPositions(name_order: Phaser.GameObjects.GameObject[]){
     )
 
     //Starting comparison
-    console.log("After sort - name_order");
-    console.log(name_order);
-    console.log("After sort -- sortedByHeight");
-    console.log(sortedByHeight);
 
     for(let i = 0; i < name_order.length; i += 1){
-        if(name_order[i].name !== sortedByHeight[i].name){
-            console.log('Printing i value');
-            console.log(name_order.indexOf(sortedByHeight[i]) - i);
+        if(name_order[i].name !== sortedByHeight[i].name){    
             return(String(`The instruction ${name_order[i]?.name}\nis out of order.`));
         }
-
     }
     return (String('All instructions are in\nthe correct location'));
 }
