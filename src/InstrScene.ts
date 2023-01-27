1202012606990acb3427d3be0e60460657dbaa43
Grace O'Leary <goleary@udel.edu>
2023-01-26 20:28:01 -1000

    Update InstrScene.ts
    Added game instructions

--- src/InstrScene.ts
+++ src/InstrScene.ts
@@ -7,8 +7,15 @@ export default class InstrScene extends Phaser.Scene {
 	}
 preload() {
     //preload here
+    this.load.image('list', 'public/assets/backgrounds/firstscene/ingredientList.png')
 }
 create() {
+    //background
+
+    const scaledRecipe = this.physics.add.image(400,225, 'list')
+    scaledRecipe.displayWidth = Number(main.config.width) * 1.2;
+    scaledRecipe.scaleY = scaledRecipe.scaleX
+
     //create here
     this.add.text(100, 200, "Welcome to Taro Mama!\nYou will cook a traditional Hawaiin meal by following \na recipe. Each level will progess in difficulty: \n\nFirst level - drag and drop the picture ingredients together\nSecond level - drag the ingredient intructions in order \nThird level - correctly order the pseudocode to create the dish.", {
         color: '0x000000'
@@ -23,6 +30,7 @@ create() {
       back.scaleY = back.scaleX;
       back.setInteractive({ useHandCursor: true });
       back.on('pointerdown', () => this.clickBack());
+    
 }
 
 clickBack() {
