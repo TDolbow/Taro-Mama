df1979a4a1561797261f16ff822cc063d1cb66b2
carleyann3091 <103849036+carleyann3091@users.noreply.github.com>
2023-01-26 22:06:09 -1000

    small instr fix

--- src/InstrScene.ts
+++ src/InstrScene.ts
@@ -17,8 +17,9 @@ create() {
     scaledRecipe.scaleY = scaledRecipe.scaleX
 
     //create here
-    this.add.text(100, 200, "Welcome to Taro Mama!\nYou will cook a traditional Hawaiin meal by following \na recipe. Each level will progess in difficulty: \n\nFirst level - drag and drop the picture ingredients together\nSecond level - drag the ingredient intructions in order \nThird level - correctly order the pseudocode to create the dish.", {
-        color: '0x000000'
+    this.add.text(100, 200, "Welcome to Taro Mama!\nYou will cook a traditional Hawaiian meal by following \na recipe. Each level will progess in difficulty: \n\nFirst level - drag and drop the picture ingredients together\nSecond level - drag the ingredient intructions in order \nThird level - correctly order the pseudocode to create the dish.", {
+        color: '0x000000',
+        align: "center"
     });
 
     const back = this.add.text(10, 500, "Back", {
