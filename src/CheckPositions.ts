fe055643c8fa1d7e106d059dfd9f18137ab9ecd1
Trevor Dolbow <tdolbow@udel.edu>
2023-01-26 23:03:42 -1000

    Adding comments to CheckPositions.ts

--- src/CheckPositions.ts
+++ src/CheckPositions.ts
@@ -1,3 +1,11 @@
+/*
+This component contains the checkPositions method, which checks the order of components against the expected order. 
+The array of objects passed in are in the expected order, and this method will check the proper order via the pass-by-reference
+property of objects in typescript. If the order is incorrect, this method will return a string that contains the incorrect command
+for feedback purposes. Otherwise, this method returns the string: `The instruction ${name_order[i]?.name}\nis out of order.`
+
+*/
+
 import Phaser from 'phaser';
 
 export function CheckPositions(name_order: Phaser.GameObjects.GameObject[]){
